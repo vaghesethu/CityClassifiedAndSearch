4fcdb2d0ba8384f4c7a5a6173fa363c51ed06c33
amv25 <77836483+amv25@users.noreply.github.com>
2021-10-12 06:34:47 +0530

    controller update

--- src/main/java/com/cityclassifiedandsearch/controller/SmtpMailSender.java
+++ src/main/java/com/cityclassifiedandsearch/controller/SmtpMailSender.java
@@ -10,28 +10,18 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SmtpMailSender {
-	
-	
-	
 	@Autowired
 	private JavaMailSender javaMailSender;	
 	
 	public void send(String to, String subject, String body) throws MessagingException {
-		
 		MimeMessage message = javaMailSender.createMimeMessage();
 		MimeMessageHelper helper;
-		
 		helper = new MimeMessageHelper(message, true); // true indicates
 													   // multipart message
 		helper.setSubject(subject);
 		helper.setTo(to);
 		helper.setText(body, true); // true indicates html
 		// continue using helper object for more functionalities like adding attachments, etc.  
-		
 		javaMailSender.send(message);
-		
-		
-		
 	}
-
 }
