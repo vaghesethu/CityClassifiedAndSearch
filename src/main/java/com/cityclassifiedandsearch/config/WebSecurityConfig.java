689f120e3b30fbde89b615e14067a2d4d1e62118
amv25 <77836483+amv25@users.noreply.github.com>
2021-10-12 15:40:07 +0530

    updated security config

--- src/main/java/com/cityclassifiedandsearch/config/WebSecurityConfig.java
+++ src/main/java/com/cityclassifiedandsearch/config/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.cityclassifiedandsearch.service.UserService;
@@ -20,6 +21,9 @@ import com.cityclassifiedandsearch.service.UserService;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	AuthenticationSuccessHandler successHandler;
 	
 	@Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -45,8 +49,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.csrf().disable().
 			authorizeRequests()
 			.antMatchers("/").anonymous()//allows users to visit page without logging in
-			.antMatchers("/admin").hasRole("ADMIN")//only admin can visit the pages with "/admin"
-			.antMatchers("/user").hasRole("USER")//only user can visit the pages with "/user"
+			.antMatchers("/admin/**").hasAuthority("ADMIN")//only admin can visit the pages with "/admin"
+			.antMatchers("/user/**").hasAuthority("USER")//only user can visit the pages with "/user"
 			.antMatchers(
 				 "/js/**",
 				 "/css/**",
@@ -59,7 +63,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 			.formLogin()
 			.loginPage("/login")//custom login page
-			.defaultSuccessUrl("/user/index", true)
+			.successHandler(successHandler)
 			.failureUrl("/login?error")
 			.permitAll()
 			.and()
